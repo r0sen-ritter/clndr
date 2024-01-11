13c517f7eba38fb3da9d495f97479c4aaeb97bc3
r0sen-ritter <safinm007@gmail.com>
2024-01-11 17:38:39 +0600

    fixed previous desc bug

--- src/components/Viewport.tsx
+++ src/components/Viewport.tsx
@@ -63,6 +63,7 @@ const Viewport: React.FC<ViewportProps> = ({ year, month }) => {
         },
       ]);
       setEventName("");
+      setEventDescription("");
       closeAddEventModal();
     }
   }
